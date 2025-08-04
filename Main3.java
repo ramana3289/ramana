a4c4bcab0a35129813fe40b8072d7e5212822afe
ramana3289 <rangamramana555@gmail.com>
2025-08-04 15:00:52 +0530

    Add files via upload

---
+++ Main3.java
@@ -0,0 +1,23 @@
+
+
+class Lamp {
+  boolean isOn;
+  void turnOn() {
+    isOn = true;
+    System.out.println("Light on? " + isOn);
+
+  }
+  void turnOff() {
+    isOn = false;
+    System.out.println("Light on? " + isOn);
+  }
+}
+
+public class Main3 {
+  public static void main(String[] args) {
+    Lamp led = new Lamp();
+    Lamp halogen = new Lamp();
+    led.turnOn();
+    halogen.turnOff();
+  }
+}
---
+++ Main5.java
@@ -0,0 +1,12 @@
+class Main5 {
+public static void main (String[] args) {
+int a=12, b=12;
+int result1 , result2;
+System.out.println("value of a:"+a);
+result1=++a;
+System.out.println("After increment:"+result1);
+System.out.println("value of b:"+b);
+result2=--b;
+System.out.println("After decrement:"+result2);
+}
+}
