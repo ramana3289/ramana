3ee3e4c37ccbc57f47120f46ee5dc31877c7948e
ramana3289 <rangamramana555@gmail.com>
2025-08-04 15:01:39 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+public static void main(String[] args)
+{
+String str="Programiz";
+boolean result;
+result=str instanceof String;
+System.out.println("Is str an object of string?" + result);
+}
+}
